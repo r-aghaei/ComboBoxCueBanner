d361f6ecb13647926a2985b40ed3b2fb37643ad1
Reza Aghaei <r.aghaei@outlook.com>
2019-11-24 04:42:52 +0800

    Update MyComboBox.cs

--- src/ComboBoxCueBanner/MyComboBox.cs
+++ src/ComboBoxCueBanner/MyComboBox.cs
@@ -11,22 +11,6 @@
             DrawMode = DrawMode.OwnerDrawFixed;
         }
         const int CB_SETCUEBANNER = 0x1703;
-        [StructLayout(LayoutKind.Sequential)]
-        public struct COMBOBOXINFO
-        {
-            public int cbSize;
-            public RECT rcItem;
-            public RECT rcButton;
-            public int stateButton;
-            public IntPtr hwndCombo;
-            public IntPtr hwndEdit;
-            public IntPtr hwndList;
-        }
-        [StructLayout(LayoutKind.Sequential)]
-        public struct RECT
-        {
-            public int Left; public int Top; public int Right; public int Bottom;
-        }
         [DllImport("user32.dll", CharSet = CharSet.Auto)]
         public static extern bool GetComboBoxInfo(IntPtr hWnd, ref COMBOBOXINFO pcbi);
 
@@ -46,12 +30,7 @@
             if (DropDownStyle == ComboBoxStyle.DropDownList)
                 this.Invalidate();
             else
-            {
-                var info = new COMBOBOXINFO();
-                info.cbSize = Marshal.SizeOf(info);
-                GetComboBoxInfo(Handle, ref info);
                 SendMessage(Handle, CB_SETCUEBANNER, 0, Hint);
-            }
         }
         protected override void OnHandleCreated(EventArgs e)
         {
